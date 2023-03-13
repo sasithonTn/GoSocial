ce99896ab8fcd9d877f5d0b009d5f6f7264b2b10
sasithont <95520117+sasithonTn@users.noreply.github.com>
2023-03-13 13:27:51 +0700

    login/register

--- routes/authenticationRoutes.js
+++ routes/authenticationRoutes.js
@@ -1,33 +1,19 @@
 const mongoose = require('mongoose');
 const Account = mongoose.model('accounts');
 
+//const argon2i = require('argon2-ffi').argon2i;
 module.exports = app => {
     //routes
-    app.get('/account', async (req, res)=>{
-        
-        const { remail, rpassword} = req.query;
+    app.post('/account/login', async (req, res)=>{
+
+        const { remail, rpassword} = req.body;
         if(remail == null || rpassword == null){
             res.send("Invalid credentails");
             return;
         }
 
         var userAccount = await Account.findOne({email: remail});
-        console.log(userAccount);
-        if(userAccount == null){
-            console.log('Creatr new account...');
-            res.send('Creatr new account...')
-            
-            /*var newAccount = new Account({
-                email : remail,
-                password : rpassword,
-
-                lastAuthentication: Date.now()
-            });
-            await newAccount.save();
-
-            res.send(newAccount);*/
-            return;
-        } else{
+        if(userAccount != null){
             if(rpassword == userAccount.password){
                 userAccount.lastAuthentication = Date.now();
                 await userAccount.save();
@@ -39,5 +25,31 @@ module.exports = app => {
         res.send("Invalid credentails");
         return;   
 
-    });    
+    });   
+    
+    app.post("/account/create", async (req, res)=>{
+        const { remail, rpassword} = req.body;
+        if(remail == null || rpassword == null){
+            res.send("Invalid credentails");
+            return;
+        }
+
+        var userAccount = await Account.findOne({email: remail});
+        if(userAccount == null){
+            console.log('Creatr new account...');            
+            var newAccount = new Account({
+                email : remail,
+                password : rpassword,
+
+                lastAuthentication: Date.now()
+            });
+            await newAccount.save();
+
+            res.send(newAccount);
+            return;
+        } else{
+            res.send("Email is already taken");
+        }
+        return; 
+    });
 }
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 //set up db
 const mongoose = require('mongoose');
