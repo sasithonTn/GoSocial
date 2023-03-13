01784e11fbec9e1658e4d2f763f81ac50eea33ed
sasithont <95520117+sasithonTn@users.noreply.github.com>
2023-03-13 17:32:55 +0700

    check email and password

--- routes/authenticationRoutes.js
+++ routes/authenticationRoutes.js
@@ -1,54 +1,113 @@
 const mongoose = require('mongoose');
 const Account = mongoose.model('accounts');
 
-//const argon2i = require('argon2-ffi').argon2i;
+const argon2i = require('argon2-ffi').argon2i;
+const crypto = require('crypto');
+
+const passwordRegex = new RegExp("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,24})")
+const emailRegex = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
 module.exports = app => {
     //routes
     app.post('/account/login', async (req, res)=>{
 
+        var response = {};
+
         const { remail, rpassword} = req.body;
-        if(remail == null || rpassword == null){
-            res.send("Invalid credentails");
+        if(remail == null || !passwordRegex.test(rpassword)){
+
+            response.code = 1;
+            response.msg = "Invalid credentails"
+            res.send(response);
             return;
         }
 
-        var userAccount = await Account.findOne({email: remail});
+        var userAccount = await Account.findOne({email: remail}, 'email password');
         if(userAccount != null){
-            if(rpassword == userAccount.password){
-                userAccount.lastAuthentication = Date.now();
-                await userAccount.save();
+            argon2i.verify(userAccount.password, rpassword).then(async (success) => {
+                if(success){
+                    userAccount.lastAuthentication = Date.now();
+                    await userAccount.save();
+
+                    response.code = 0;
+                    response.msg = "Account found"
+                    response.data = ( ({email}) => ({ email}) )(userAccount);
+                    res.send(response);
+                    return;
+                }else{
+                    response.code = 1;
+                    response.msg = "Invalid credentails"
+                    res.send(response);
+                    return; 
+                }
                 
-                res.send(userAccount);
-                return;
-            }
+            });
+        }else{
+            response.code = 1;
+            response.msg = "Invalid credentails"
+            res.send(response);
+            return;
         }
-        res.send("Invalid credentails");
-        return;   
+          
 
     });   
     
     app.post("/account/create", async (req, res)=>{
+
+        var response = {};
+
         const { remail, rpassword} = req.body;
-        if(remail == null || rpassword == null){
-            res.send("Invalid credentails");
+        if(remail == null || !emailRegex.test(remail)){
+            response.code = 1;
+            response.msg = "Invalid credentails"
+            res.send(response);
             return;
         }
 
-        var userAccount = await Account.findOne({email: remail});
-        if(userAccount == null){
-            console.log('Creatr new account...');            
-            var newAccount = new Account({
-                email : remail,
-                password : rpassword,
-
-                lastAuthentication: Date.now()
-            });
-            await newAccount.save();
-
-            res.send(newAccount);
+        if(!passwordRegex.test(rpassword)){
+            response.code = 3;
+            response.msg = "Unsafe password"
+            res.send(response);
             return;
+        }
+
+        if(!emailRegex.test(remail)){
+            response.code = 4;
+            response.msg = "Invalid email format"
+            res.send(response);
+            return;
+        }
+
+        var userAccount = await Account.findOne({email: remail},'_id');
+        if(userAccount == null){
+            console.log('Creatr new account...'); 
+            
+            //Generate a unique access token
+            crypto.randomBytes(32, function(err, salt){
+                if(err){
+                    console.log(err);
+                }
+
+                 argon2i.hash(rpassword, salt).then(async (hash) =>{
+                    var newAccount = new Account({
+                        email : remail,
+                        password : hash,
+                        salt: salt,
+        
+                        lastAuthentication: Date.now()
+                    });
+                    await newAccount.save();
+
+                    response.code = 0;
+                    response.msg = "Account found"
+                    response.data = ( ({email}) => ({ email}) )(newAccount);
+                    res.send(response);
+                    return;
+                });
+            });
         } else{
-            res.send("Email is already taken");
+            response.code = 2;
+            response.msg = "Email is already taken"
+            res.send(response);
         }
         return; 
     });
