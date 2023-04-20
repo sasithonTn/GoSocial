f2c6ea32ad4ff163e5befaa248b3165f5f5461f6
sasithont <95520117+sasithonTn@users.noreply.github.com>
2023-04-20 12:22:19 +0700

    object

--- server.js
+++ server.js
@@ -11,10 +11,14 @@ const url = 'mongodb+srv://goSocail:myamtan@cluster0.ypcqdkb.mongodb.net/GoSocia
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true,});
 
 //setup db models
-require('./model/Account.js')
+require('./model/Account.js');
+require('./model/Object.js');
+//require('./model/User_objects.js');
 
 //setup routes
 require('./routes/authenticationRoutes.js')(app);
+require('./routes/objectRoutes.js')(app);
+//require('./routes/userObjRoutes.js')(app);
 
 app.listen(3000, ()=>{
     console.log("listenind on 3000");
