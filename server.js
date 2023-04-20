7650b4e28eaeb2ac31ac471c8eaced2803258d7d
sasithont <95520117+sasithonTn@users.noreply.github.com>
2023-04-20 17:29:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const mongoose = require('mongoose');
 const url = 'mongodb+srv://goSocail:myamtan@cluster0.ypcqdkb.mongodb.net/GoSocial'
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true,});
 
-
 //setup db models
 require('./model/Account.js');
 require('./model/Object.js');
-//require('./model/User_objects.js');
+require('./model/User_objects.js');
+require('./mmodel/Object_position.js');
 
 require('./model/focus_time.js')
 require('./model/tag.js')
@@ -24,11 +24,12 @@ require('./model/quest.js')
 //setup routes
 require('./routes/authenticationRoutes.js')(app);
 require('./routes/objectRoutes.js')(app);
-//require('./routes/userObjRoutes.js')(app);
+require('./routes/userObjRoutes.js')(app);
 require('./routes/focus_timeRoutes.js')(app);
 require('./routes/tag_Routes.js')(app);
 require('./routes/quest_Routes.js')(app);
 require('./routes/quest_timeRoutes.js')(app);
+require('./routes/objectPositionRoutes.js')(app);
 
 app.listen(3000, ()=>{
     console.log("listenind on 3000");
