b154e68c0e1efed0012eef566b7f514eb033c48c
sasithont <95520117+sasithonTn@users.noreply.github.com>
2023-06-20 16:53:44 +0700

    ดึงไอเทมเข้าห้อง

--- server.js
+++ server.js
@@ -14,27 +14,25 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true,});
 //setup db models
 require('./model/Account.js');
 require('./model/User_item.js');
-require('./model/User_objects.js');
-require('./model/Object_position.js');
-
+require('./model/Room.js');
 require('./model/focus_time.js');
 require('./model/tag.js');
 require('./model/Interests.js');
 require('./model/User_interests.js');
 require('./model/Score.js');
+require('./model/Inventory.js');
 
 //setup routes
 require('./routes/authenticationRoutes.js')(app);
-require('./routes/userObjRoutes.js')(app);
 require('./routes/focus_timeRoutes.js')(app);
 require('./routes/tag_Routes.js')(app);
-require('./routes/objectPositionRoutes.js')(app);
 require('./routes/interestRoutes.js')(app);
 require('./routes/user_interestRoutes.js')(app);
 require('./routes/user_itemRoutes.js')(app);
 require('./routes/contentBaseFiltering.js')(app);
 require('./routes/score_Routes.js')(app);
-
+require('./routes/room_Routes.js')(app);
+require('./routes/Inventory_Routes.js')(app);
 app.listen(3000, ()=>{
     console.log("listenind on 3000");
 });
